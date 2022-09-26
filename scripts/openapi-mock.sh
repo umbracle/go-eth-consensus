4d5e1f5c1d11fa91cc0f2a0dcd252dfc48a51237
Ferran Borreguero <ferranbt@protonmail.com>
2022-09-26 20:36:12 +0200

    Fix ci

--- scripts/openapi-mock.sh
+++ scripts/openapi-mock.sh
@@ -5,3 +5,6 @@ docker run --init -d -p 4010:4010 stoplight/prism:4 mock -h 0.0.0.0 https://gith
 
 # Start builder api mock
 docker run --init -d -p 4011:4010 stoplight/prism:4 mock -h 0.0.0.0 https://github.com/ethereum/builder-specs/releases/download/v0.2.0/builder-oapi.yaml
+
+# wait for the endpoints to be available
+sleep 5
