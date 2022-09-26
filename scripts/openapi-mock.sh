37e2348719aeaf9a00717b0bb4eeb7d5b15cd5c0
Ferran Borreguero <ferranbt@protonmail.com>
2022-09-26 21:12:02 +0200

    reset3

--- scripts/openapi-mock.sh
+++ scripts/openapi-mock.sh
@@ -4,7 +4,7 @@
 docker run --init --name prism-beacon -d -p 4010:4010 stoplight/prism:4 mock -h 0.0.0.0 https://github.com/ethereum/beacon-APIs/releases/download/v2.3.0/beacon-node-oapi.json
 
 # Wait for beacon mock api
-#grep -m 1 "Prism is listening" <(docker logs prism-beacon -f)
+grep -m 1 "Prism is listening" <(docker logs prism-beacon -f)
 
 # Start builder api mock
 docker run --init --name prism-builder -d -p 4011:4010 stoplight/prism:4 mock -h 0.0.0.0 https://github.com/ethereum/builder-specs/releases/download/v0.2.0/builder-oapi.yaml
